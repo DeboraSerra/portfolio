531869c7a8fe48c20dd7e3e53e5f34fe041af444
Débora Serra <debora.r.serra@gmail.com>
2022-03-24 20:03:51 -0300

    Updates

---
+++ precache-manifest.e656317e920bb4bc32d853a149ff92f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d06998bc4270073bceb846b28cafa22",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd41f2a1ed56a4f66d58",
+    "url": "/portfolio/static/css/main.81fb00b0.chunk.css"
+  },
+  {
+    "revision": "5cf0bef3bfac57f4b249",
+    "url": "/portfolio/static/js/2.90f16533.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.90f16533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd41f2a1ed56a4f66d58",
+    "url": "/portfolio/static/js/main.8e45a1f9.chunk.js"
+  },
+  {
+    "revision": "0ed2dceec2199c9570e6",
+    "url": "/portfolio/static/js/runtime-main.02268697.js"
+  },
+  {
+    "revision": "dc15c8362ea0e1873b115f0e97fad9bc",
+    "url": "/portfolio/static/media/Brasília.dc15c836.jpeg"
+  },
+  {
+    "revision": "f3f519dcde694e21a8689e93168fb26e",
+    "url": "/portfolio/static/media/Fred.f3f519dc.jpg"
+  },
+  {
+    "revision": "1b37eafb3c4a6fb7dc2a42eecb32dfb9",
+    "url": "/portfolio/static/media/Perfil.1b37eafb.jpeg"
+  },
+  {
+    "revision": "38ea35c7127e1bb3519f93db465ada5b",
+    "url": "/portfolio/static/media/Resume-Débora-Serra.38ea35c7.pdf"
+  }
+]);
