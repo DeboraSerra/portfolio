45e6ba875f72f1bbc19482bdbb941892c3f18b10
Débora Serra <debora.r.serra@gmail.com>
2022-04-01 17:35:20 -0300

    Add projects' pictures and refactor services

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { projectLink, projectText, projectId } from '../services/script';
+import projects from '../services/script';
 import '../styles/Projects.css';
 
 class Projects extends React.Component {
+
+  renderImg = (src, name) => {
+    return <img src={ src } alt={ name } />
+  }
 
   render() {
     return (
       <section className="projects-sect">
         <h2 className="projects-title">My Projects</h2>
         <ul className="projects-list">
-          {projectLink.map((project, index) => (
-            <a className="project-link" href={ project }>
-              <li className="project-item" key={ projectId[index] }>
-                  {projectText[index]}
+          {projects.map(({ id, name, link, src }) => (
+            <a className="project-link" href={ link }>
+              <li
+                className="project-item"
+                key={ id }
+              >
+                {src && <img className="project-img" src={ src } alt={ name } width="100" />}
+                <p className="project-name">{name}</p>
               </li>
             </a>
           ))}
