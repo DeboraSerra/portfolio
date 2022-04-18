612cea34c1bc41f083055df79be185a09f674f6d
Débora Serra <debora.r.serra@gmail.com>
2022-04-18 17:57:16 -0300

    Update projects' page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,19 +12,57 @@ class Projects extends React.Component {
     return (
       <section className="projects-sect">
         <h2 className="projects-title">My Projects</h2>
-        <ul className="projects-list">
-          {projects.map(({ id, name, link, src }) => (
-            <a className="project-link" href={ link }>
-              <li
-                className="project-item"
-                key={ id }
-              >
-                {src && <img className="project-img" src={ src } alt={ name } />}
-                <p className="project-name">{name}</p>
-              </li>
-            </a>
-          ))}
-        </ul>
+        <h3 className="projects-module">Fundaments</h3>
+        <section className="projects-list">
+          {projects.map(({ id, module, name, link, src }) => {
+            if (module === "fundaments") {
+              return (
+                <a className="project-link" href={link}>
+                  <li className="project-item" key={id}>
+                    {src && (
+                      <img className="project-img" src={src} alt={name} />
+                    )}
+                    <p className="project-name">{name}</p>
+                  </li>
+                </a>
+              );
+            }
+          })}
+        </section>
+        <h3 className="projects-module">Font End Development</h3>
+        <section className="projects-list">
+          {projects.map(({ id, module, name, link, src }) => {
+            if (module === "frontEnd") {
+              return (
+                <a className="project-link" href={link}>
+                  <li className="project-item" key={id}>
+                    {src && (
+                      <img className="project-img" src={src} alt={name} />
+                    )}
+                    <p className="project-name">{name}</p>
+                  </li>
+                </a>
+              );
+            }
+          })}
+        </section>
+        <h3 className="projects-module">Personal Projects</h3>
+        <section className="projects-list">
+          {projects.map(({ id, module, name, link, src }) => {
+            if (module === "personal") {
+              return (
+                <a className="project-link" href={link}>
+                  <li className="project-item" key={id}>
+                    {src && (
+                      <img className="project-img" src={src} alt={name} />
+                    )}
+                    <p className="project-name">{name}</p>
+                  </li>
+                </a>
+              );
+            }
+          })}
+        </section>
       </section>
     );
   };
