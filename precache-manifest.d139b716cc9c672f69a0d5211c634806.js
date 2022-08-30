92eceea9f9109b53475fa2bdf2c1d8ab739d0bf9
debora <debora.r.serra@gmail.com>
2022-08-30 18:06:51 -0300

    Updates

--- precache-manifest.d139b716cc9c672f69a0d5211c634806.js
+++ precache-manifest.d139b716cc9c672f69a0d5211c634806.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbb7996825ef632f43b109bdf6aa11af",
+    "revision": "fcf41af90edc775821008865c8ab795f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "505e97db0f75e2741974",
-    "url": "/portfolio/static/css/main.ee210cf1.chunk.css"
+    "revision": "e34eeee0e2a1d39de79a",
+    "url": "/portfolio/static/css/main.93fb1cab.chunk.css"
   },
   {
-    "revision": "57e6c48e3838283c3074",
-    "url": "/portfolio/static/js/2.2a354f22.chunk.js"
+    "revision": "1af6bd0f8882d89ac95e",
+    "url": "/portfolio/static/js/2.34d4edf3.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/portfolio/static/js/2.2a354f22.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.34d4edf3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "505e97db0f75e2741974",
-    "url": "/portfolio/static/js/main.7c1362e1.chunk.js"
+    "revision": "e34eeee0e2a1d39de79a",
+    "url": "/portfolio/static/js/main.f89fcb54.chunk.js"
   },
   {
     "revision": "0ed2dceec2199c9570e6",
@@ -32,7 +32,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/Perfil.1b37eafb.jpeg"
   },
   {
-    "revision": "06f3abb29868dfd8b56204be83cff19c",
-    "url": "/portfolio/static/media/Resume-Débora-Serra.06f3abb2.pdf"
+    "revision": "597b1981a208e90c6b898942b3cac527",
+    "url": "/portfolio/static/media/Resume-Débora-Serra.597b1981.pdf"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.687d8ffbaf6d6955a7c3f2b3817fe324.js"
+  "/portfolio/precache-manifest.d139b716cc9c672f69a0d5211c634806.js"
 );
 
 self.addEventListener('message', (event) => {
